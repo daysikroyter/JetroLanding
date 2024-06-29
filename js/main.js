dbbe88839b7eae189a04036101ccc3b0e9de3b23
daysikroyter <nubchenko@gmail.com>
2024-06-29 18:13:28 +0300

    added header & main

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(function () {
+  $('.top__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/next-arrow.png" alt="arrow-prev"></button>',
+    nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/next-arrow.png" alt="arrow-prev"></button>',
+  });
 
+  $('.top__items').slick({
+    asNavFor: '.top__slider',
+    slidesToShow: 6,
+    focusOnSelect: true,
+  });
 });
