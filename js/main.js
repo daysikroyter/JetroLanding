36f355f0954f1b9b678caad4c8c877f40b660a27
daysikroyter <nubchenko@gmail.com>
2024-06-29 21:14:30 +0300

    added responsive design

--- js/main.js
+++ js/main.js
@@ -11,5 +11,32 @@ $(function () {
     asNavFor: '.top__slider',
     slidesToShow: 6,
     focusOnSelect: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          autoplay: true
+        }
+      },
+      {
+        breakpoint: 569,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 481,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+    ]
+  });
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').slideToggle();
   });
 });
